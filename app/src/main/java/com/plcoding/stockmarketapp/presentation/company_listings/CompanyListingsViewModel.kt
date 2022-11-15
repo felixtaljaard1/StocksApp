9235206a5fc3cdb9c05112499912408489486051
felixtaljaard1 <felix.taljaard@informationtechconsultants.co.uk>
2022-11-15 10:10:58 +0000

    first

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsViewModel.kt
@@ -0,0 +1,56 @@
+package com.plcoding.stockmarketapp.presentation.company_listings
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.stockmarketapp.domain.repository.StockRepository
+import com.plcoding.stockmarketapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CompanyListingsViewModel @Inject constructor(
+    private val repository: StockRepository
+) : ViewModel() {
+    var state by mutableStateOf(CompanyListingsState())
+
+    fun onEvent(event: CompanyListingsEvent) {
+        when (event) {
+            is CompanyListingsEvent.Refresh -> {
+                getCompanyListings(fetchFromRemote = true)
+            }
+            is CompanyListingsEvent.OnSearchQueryChange -> {
+
+            }
+        }
+    }
+
+    fun getCompanyListings(
+        query: String = state.searchQuery.lowercase(),
+        fetchFromRemote: Boolean = false
+    ) {
+        viewModelScope.launch {
+            repository
+                .getCompanyListings(fetchFromRemote, query)
+                .collect { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            result.data?.let { listings ->
+                                state = state.copy(
+                                    companies = listings
+                                )
+                            }
+                        }
+                        is Resource.Error -> Unit
+                        is Resource.Loading -> {
+                            state = state.copy(isLoading = result.isLoading)
+                        }
+                    }
+
+                }
+        }
+    }
+}
